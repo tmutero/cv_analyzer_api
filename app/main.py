e42a889d09d0036fc2ec1c8bbdb527bec8e95643
Tafadzwa Mutero <tafadzwatendekaimutero@gmail.com>
2025-02-06 09:32:36 +0200

    Document API class with openapi tags

--- app/main.py
+++ app/main.py
@@ -5,8 +5,18 @@ from fastapi.staticfiles import StaticFiles
 from app import __version__
 from app.routers.api_router import api_router
 from app.settings import settings
+tags_metadata = [
+    {
+        "name": "User",
+        "description": "Operations with users. The **login** logic is also here.",
+    },
+    {
+        "name": "CV Analyzer",
+        "description": "API that uploads CV and provides a concise summary and improvement of the CV contents.",
 
-app = FastAPI(title=settings.PROJECT_NAME, version=__version__)
+    }
+]
+app = FastAPI(title=settings.PROJECT_NAME, version=__version__,openapi_tags=tags_metadata)
 
 app.add_middleware(
     CORSMiddleware,
